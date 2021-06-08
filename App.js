a95e3d22ee094b4bfcab385100c8ddaac593973b
yashraj021 <yash.raj021@gmail.com>
2021-06-09 00:19:25 +0530

    Created persisted restaurant favourites component. Added firebase.

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
 import {
@@ -9,22 +9,49 @@ import {
   useFonts as useFontsLato,
   Lato_400Regular,
 } from "@expo-google-fonts/lato";
+import * as firebase from "firebase";
 
 import { theme } from "./src/infrastructure/theme/index";
-
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
-
 import { Navigator } from "./src/infrastructure/navigation/index";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBavAnUw2qI5mGlOyD0fHdsYG0kRI-EYAY",
+  authDomain: "mealstogo-b17ee.firebaseapp.com",
+  projectId: "mealstogo-b17ee",
+  storageBucket: "mealstogo-b17ee.appspot.com",
+  messagingSenderId: "797732271029",
+  appId: "1:797732271029:web:3f6f9ea485eee8467a9ffc",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword("email", "password")
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   const [oswladLoaded] = useFontsOswald({ Oswald_400Regular });
   const [latoLoaded] = useFontsLato({ Lato_400Regular });
 
   if (!oswladLoaded || !latoLoaded) {
     return null;
   }
+
+  if (!isAuthenticated) return null;
 
   return (
     <>
--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,10 +1,31 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect } from "react";
 import { useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = ({ children }) => {
   const [favourites, setFavourites] = useState([]);
+
+  const saveFavourites = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem("@favourites", jsonValue);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const loadFavourites = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@favourites");
+      if (value !== null) {
+        setFavourites(JSON.parse(value));
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const add = (restaurant) => {
     setFavourites([...favourites, restaurant]);
@@ -17,6 +38,14 @@ export const FavouritesContextProvider = ({ children }) => {
 
     setFavourites(newFavourites);
   };
+
+  useEffect(() => {
+    loadFavourites();
+  }, []);
+
+  useEffect(() => {
+    saveFavourites(favourites);
+  }, [favourites]);
 
   return (
     <FavouritesContext.Provider
